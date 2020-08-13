6deaacc6ab6438cbff5cecd37cd83cf9a25f4562
kaumadi-op <68402650+kaumadi-op@users.noreply.github.com>
2020-08-14 00:59:52 +0530

    Menu management function added.

---
+++ app/src/main/java/com/example/mealfactoryapp/MenuSl.java
@@ -0,0 +1,14 @@
+package com.example.mealfactoryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuSl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_sl);
+    }
+}
