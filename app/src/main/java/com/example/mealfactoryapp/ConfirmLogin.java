16b8856141c9af7cc1d0013cd6019eea2161490a
kaumadi-op <68402650+kaumadi-op@users.noreply.github.com>
2020-08-13 14:00:23 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/mealfactoryapp/ConfirmLogin.java
@@ -0,0 +1,14 @@
+package com.example.mealfactoryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfirmLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_login);
+    }
+}
