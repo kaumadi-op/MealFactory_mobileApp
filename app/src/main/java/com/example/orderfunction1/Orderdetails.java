73e6d74bbeda4d3a30ef61066a4ec808a9c18258
asus <amandimaheshavi@gmail.com>
2020-08-15 09:57:57 -0700

    Initial commit

--- app/src/main/java/com/example/orderfunction1/Orderdetails.java
+++ app/src/main/java/com/example/orderfunction1/Orderdetails.java
@@ -1,4 +1,14 @@
 package com.example.orderfunction1;
 
-public class Orderdetails {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Orderdetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orderdetails);
+    }
 }
