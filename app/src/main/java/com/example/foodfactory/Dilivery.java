10fe8473257ecff8a7efa09b08d93e253cb7cc52
lakrandii <it19136202@my.sliit.lk>
2020-08-15 22:09:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/foodfactory/Dilivery.java
@@ -0,0 +1,14 @@
+package com.example.foodfactory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dilivery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dilivery);
+    }
+}
---
+++ app/src/main/java/com/example/foodfactory/paymentMethod.java
@@ -0,0 +1,14 @@
+package com.example.foodfactory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class paymentMethod extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paymentmethod);
+    }
+}
