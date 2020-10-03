2afc159b3060de4a6bc76a96db2a02dd4e2c18b5
Kaumadi <k.omesha99@gmail.com>
2020-10-04 02:13:22 +0530

    Admin Insertion removed & Favorite page modified

--- app/src/main/java/com/example/mealfactoryapp/Favorites.java
+++ app/src/main/java/com/example/mealfactoryapp/Favorites.java
@@ -2,13 +2,84 @@ package com.example.mealfactoryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Favorites extends AppCompatActivity {
+
+    ImageButton home_btn, bag_btn, favorite_btn, profile_btn, add_btn, dlt_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+
+        home_btn = findViewById(R.id.home_btn);
+        bag_btn = findViewById(R.id.bag_btn);
+        favorite_btn = findViewById(R.id.favorite_btn);
+        profile_btn = findViewById(R.id.profile_btn);
+        add_btn = findViewById(R.id.add_btn);
+        dlt_btn = findViewById(R.id.dlt_btn);
+
+        home_btn.setOnClickListener(new View.OnClickListener(){
+
+                                        @Override
+                                        public void onClick(View v){
+
+                                            Intent Intent = new Intent (Favorites.this,MenuSl.class);
+                                            startActivity(Intent);
+
+                                            Log.i("lifecycle", "onClick method invoked");
+
+                                        }
+                                    }
+        );
+
+        bag_btn.setOnClickListener(new View.OnClickListener(){
+
+                                       @Override
+                                       public void onClick(View v){
+
+                                           Intent Intent = new Intent (Favorites.this,MenuSl.class);
+                                           startActivity(Intent);
+
+                                           Log.i("lifecycle", "onClick method invoked");
+
+                                       }
+                                   }
+        );
+
+        favorite_btn.setOnClickListener(new View.OnClickListener(){
+
+                                            @Override
+                                            public void onClick(View v){
+
+                                                Intent Intent = new Intent (Favorites.this,Favorites.class);
+                                                startActivity(Intent);
+
+                                                Log.i("lifecycle", "onClick method invoked");
+
+                                            }
+                                        }
+        );
+
+        profile_btn.setOnClickListener(new View.OnClickListener(){
+
+                                           @Override
+                                           public void onClick(View v){
+
+                                               Intent Intent = new Intent (Favorites.this,MyProfile.class);
+                                               startActivity(Intent);
+
+                                               Log.i("lifecycle", "onClick method invoked");
+
+                                           }
+                                       }
+        );
+
     }
 }
